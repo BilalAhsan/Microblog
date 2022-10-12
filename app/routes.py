7a8c9e0c6cb83772a637be6b4a584dd8310fcee9
BilalAhsan <bilal200444@gmail.com>
2022-10-12 17:42:09 +0500

    Application structure

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from langdetect import detect, LangDetectException
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
+from app.translate import translate
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
@@ -17,6 +19,20 @@ from app.models import User, Post
 from app.email import send_password_reset_email
 
 
+@app.route("/translate", methods=["POST"])
+@login_required
+def translate_text():
+    return jsonify(
+        {
+            "text": translate(
+                request.form["text"],
+                request.form["source_language"],
+                request.form["dest_language"],
+            )
+        }
+    )
+
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
@@ -31,7 +47,11 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ""
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash(_("Your post is now live!"))
